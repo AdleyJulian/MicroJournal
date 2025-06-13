1a14dcd538123d7dc8b4545a2bbf283810502ac4
AdleyJulian <AdleyJulian@gmail.com>
2025-06-12 22:14:25 -0500

    android prebuild 	modified:   android/app/build.gradle 	renamed:    android/app/src/main/java/com/anonymous/Pensieve/MainActivity.kt -> android/app/src/main/java/com/anonymous/microjournal/MainActivity.kt 	renamed:    android/app/src/main/java/com/anonymous/Pensieve/MainApplication.kt -> android/app/src/main/java/com/anonymous/microjournal/MainApplication.kt

--- android/app/src/main/java/com/anonymous/microjournal/MainActivity.kt
+++ android/app/src/main/java/com/anonymous/microjournal/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.Pensieve
+package com.anonymous.microjournal
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/anonymous/microjournal/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/microjournal/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.Pensieve
+package com.anonymous.microjournal
 
 import android.app.Application
 import android.content.res.Configuration
